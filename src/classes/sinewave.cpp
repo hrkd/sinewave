48e9c272e6b5b0cd3a6a3a732f11a69463f18a15
hrkd <hiroki@hrkd.net>
2022-04-22 16:08:05 +0900

    :technologist: ready to build

---
+++ src/classes/sinewave.cpp
@@ -0,0 +1,18 @@
+//
+//  sinewave.cpp
+//  sinewave
+//
+//  Created by @_hrkd on 2022/04/22.
+//  
+//
+
+#include "sinewave.hpp"
+void sinewave::setup(){
+
+}
+void sinewave::update(){
+
+}
+void sinewave::draw(){
+
+}
---
+++ src/classes/sinewave.hpp
@@ -0,0 +1,25 @@
+//
+//  sinewave.hpp
+//  sinewave
+//
+//  Created by @_hrkd on 2022/04/22.
+//  
+//
+
+#ifndef sinewave_hpp
+#define sinewave_hpp
+
+#include <stdio.h>
+
+#include "ofMain.h"
+
+class sinewave : public ofBaseApp {
+public:
+    void setup();
+    void update();
+    void draw();
+private:
+
+};
+
+#endif /* sinewave_hpp */
